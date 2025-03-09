491081e50536c3da206acdff7776df1fd3e5fdc6
h-shamamyan <shamamyanhovsep@gmail.com>
2025-03-09 21:58:52 +0400

    Update PrintASquareOfStars.js

--- Lesson5 - Loops/PrintASquareOfStars.js
+++ Lesson5 - Loops/PrintASquareOfStars.js
@@ -1,4 +1,4 @@
-const prompt=require("prompt-sync")({sigint:true});
+const prompt=require("prompt-sync")({sigint:true}); // in all of your files you have prompt-sync imported but you don't use it, if you don't use it you shouldn't import it
 
 const size = 4;
 for (let i = 0; i < size; i++) {
